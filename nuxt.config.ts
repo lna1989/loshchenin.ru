4ef1e5a7fb44fc1a83c58df519317a59e39214ba
Nikita Loshchenin <nikita@loshchenin.ru>
2023-10-22 05:35:00 +0400

    add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/i18n',
+    ],
+    i18n: {
+        strategy: 'prefix_except_default',
+        defaultLocale: 'ru',
+        langDir: 'i18n',
+        locales: [
+            {
+                code: 'en',
+                iso: 'en-US',
+                file: 'en.json'
+            },
+            {
+                code: 'ru',
+                iso: 'ru-RU',
+                file: 'ru.json'
+            }
+        ],
+        lazy: true,
+    }
 })
